8ee9b4f886c92c06afe39449c899ddd517991e8c
Abdalrhman jaffar <144527872+Abdalrhman-M-Jaffar@users.noreply.github.com>
2024-10-06 22:24:19 +0300

    Upload-1

---
+++ control.js
@@ -0,0 +1,323 @@
+let counter = 1;
+let item1 = document.getElementById('item1');
+let item2 = document.getElementById('item2');
+let item3 = document.getElementById('item3');
+let item4 = document.getElementById('item4');
+let item5 = document.getElementById('item5');
+let item6 = document.getElementById('item6');
+let item7 = document.getElementById('item7');
+let item8 = document.getElementById('item8');
+let item9 = document.getElementById('item9');
+
+item1.onmouseenter = function(){
+    counter = 1;
+    itemTitle = document.getElementById(`itemTitle${counter}`);
+    price = document.getElementById(`price${counter}`);
+    count = document.getElementById(`count${counter}`);
+    addToCartStyle = document.getElementById(`addToCartStyle${counter}`);
+    countBtnStyle = document.getElementById(`countBtnStyle${counter}`);
+    amount = document.getElementById(`amount${counter}`);
+    done = document.getElementById(`done${counter}`);
+    minesSign = document.getElementById(`mines-sign${counter}`);
+    plusSign = document.getElementById(`plus-sign${counter}`);
+    addToCart = document.getElementById(`addToCart${counter}`);
+    // console.log(itemTitle, price, count, addToCartStyle, countBtnStyle, amount, done, minesSign, plusSign, addToCart)
+}
+item2.onmouseenter = function(){
+    counter = 2;
+    itemTitle = document.getElementById(`itemTitle${counter}`);
+    price = document.getElementById(`price${counter}`);
+    count = document.getElementById(`count${counter}`);
+    addToCartStyle = document.getElementById(`addToCartStyle${counter}`);
+    countBtnStyle = document.getElementById(`countBtnStyle${counter}`);
+    amount = document.getElementById(`amount${counter}`);
+    done = document.getElementById(`done${counter}`);
+    minesSign = document.getElementById(`mines-sign${counter}`);
+    plusSign = document.getElementById(`plus-sign${counter}`);
+    addToCart = document.getElementById(`addToCart${counter}`);
+    // console.log(itemTitle, price, count, addToCartStyle, countBtnStyle, amount, done, minesSign, plusSign, addToCart)
+}
+item3.onmouseenter = function(){
+    counter = 3;
+    itemTitle = document.getElementById(`itemTitle${counter}`);
+    price = document.getElementById(`price${counter}`);
+    count = document.getElementById(`count${counter}`);
+    addToCartStyle = document.getElementById(`addToCartStyle${counter}`);
+    countBtnStyle = document.getElementById(`countBtnStyle${counter}`);
+    amount = document.getElementById(`amount${counter}`);
+    done = document.getElementById(`done${counter}`);
+    minesSign = document.getElementById(`mines-sign${counter}`);
+    plusSign = document.getElementById(`plus-sign${counter}`);
+    addToCart = document.getElementById(`addToCart${counter}`);
+    // console.log(itemTitle, price, count, addToCartStyle, countBtnStyle, amount, done, minesSign, plusSign, addToCart)
+}
+item4.onmouseenter = function(){
+    counter = 4;
+    itemTitle = document.getElementById(`itemTitle${counter}`);
+    price = document.getElementById(`price${counter}`);
+    count = document.getElementById(`count${counter}`);
+    addToCartStyle = document.getElementById(`addToCartStyle${counter}`);
+    countBtnStyle = document.getElementById(`countBtnStyle${counter}`);
+    amount = document.getElementById(`amount${counter}`);
+    done = document.getElementById(`done${counter}`);
+    minesSign = document.getElementById(`mines-sign${counter}`);
+    plusSign = document.getElementById(`plus-sign${counter}`);
+    addToCart = document.getElementById(`addToCart${counter}`);
+    // console.log(itemTitle, price, count, addToCartStyle, countBtnStyle, amount, done, minesSign, plusSign, addToCart)
+}
+item5.onmouseenter = function(){
+    counter = 5;
+    itemTitle = document.getElementById(`itemTitle${counter}`);
+    price = document.getElementById(`price${counter}`);
+    count = document.getElementById(`count${counter}`);
+    addToCartStyle = document.getElementById(`addToCartStyle${counter}`);
+    countBtnStyle = document.getElementById(`countBtnStyle${counter}`);
+    amount = document.getElementById(`amount${counter}`);
+    done = document.getElementById(`done${counter}`);
+    minesSign = document.getElementById(`mines-sign${counter}`);
+    plusSign = document.getElementById(`plus-sign${counter}`);
+    addToCart = document.getElementById(`addToCart${counter}`);
+    // console.log(itemTitle, price, count, addToCartStyle, countBtnStyle, amount, done, minesSign, plusSign, addToCart)
+}
+item6.onmouseenter = function(){
+    counter = 6;
+    itemTitle = document.getElementById(`itemTitle${counter}`);
+    price = document.getElementById(`price${counter}`);
+    count = document.getElementById(`count${counter}`);
+    addToCartStyle = document.getElementById(`addToCartStyle${counter}`);
+    countBtnStyle = document.getElementById(`countBtnStyle${counter}`);
+    amount = document.getElementById(`amount${counter}`);
+    done = document.getElementById(`done${counter}`);
+    minesSign = document.getElementById(`mines-sign${counter}`);
+    plusSign = document.getElementById(`plus-sign${counter}`);
+    addToCart = document.getElementById(`addToCart${counter}`);
+    // console.log(itemTitle, price, count, addToCartStyle, countBtnStyle, amount, done, minesSign, plusSign, addToCart)
+}
+item7.onmouseenter = function(){
+    counter = 7;
+    itemTitle = document.getElementById(`itemTitle${counter}`);
+    price = document.getElementById(`price${counter}`);
+    count = document.getElementById(`count${counter}`);
+    addToCartStyle = document.getElementById(`addToCartStyle${counter}`);
+    countBtnStyle = document.getElementById(`countBtnStyle${counter}`);
+    amount = document.getElementById(`amount${counter}`);
+    done = document.getElementById(`done${counter}`);
+    minesSign = document.getElementById(`mines-sign${counter}`);
+    plusSign = document.getElementById(`plus-sign${counter}`);
+    addToCart = document.getElementById(`addToCart${counter}`);
+    // console.log(itemTitle, price, count, addToCartStyle, countBtnStyle, amount, done, minesSign, plusSign, addToCart)
+}
+item8.onmouseenter = function(){
+    counter = 8;
+    itemTitle = document.getElementById(`itemTitle${counter}`);
+    price = document.getElementById(`price${counter}`);
+    count = document.getElementById(`count${counter}`);
+    addToCartStyle = document.getElementById(`addToCartStyle${counter}`);
+    countBtnStyle = document.getElementById(`countBtnStyle${counter}`);
+    amount = document.getElementById(`amount${counter}`);
+    done = document.getElementById(`done${counter}`);
+    minesSign = document.getElementById(`mines-sign${counter}`);
+    plusSign = document.getElementById(`plus-sign${counter}`);
+    addToCart = document.getElementById(`addToCart${counter}`);
+    // console.log(itemTitle, price, count, addToCartStyle, countBtnStyle, amount, done, minesSign, plusSign, addToCart)
+}
+item9.onmouseenter = function(){
+    counter = 9;
+    itemTitle = document.getElementById(`itemTitle${counter}`);
+    price = document.getElementById(`price${counter}`);
+    count = document.getElementById(`count${counter}`);
+    addToCartStyle = document.getElementById(`addToCartStyle${counter}`);
+    countBtnStyle = document.getElementById(`countBtnStyle${counter}`);
+    amount = document.getElementById(`amount${counter}`);
+    done = document.getElementById(`done${counter}`);
+    minesSign = document.getElementById(`mines-sign${counter}`);
+    plusSign = document.getElementById(`plus-sign${counter}`);
+    addToCart = document.getElementById(`addToCart${counter}`);
+    // console.log(itemTitle, price, count, addToCartStyle, countBtnStyle, amount, done, minesSign, plusSign, addToCart)
+}
+
+// variables
+var itemTitle;
+var price;
+var count; // input
+var addToCart;
+var addToCartStyle;
+var countBtnStyle;
+var amount;
+var done;
+var minesSign;
+var plusSign;
+var tmpCountSumer;
+
+var emptyCart = document.getElementById('emptyCart');
+var cartWithOrders = document.getElementById('cart-with-orders');
+
+// add to cart btns design
+function stylingBtnAmount(){
+    addToCartStyle.style.cssText = `z-index: 50; opacity: 0;`;
+    countBtnStyle.style.cssText = `z-index: 52; opacity: 1;`;
+}
+
+function stylingBtnDone(){
+    addToCartStyle.style.cssText = `z-index: 52; opacity: 1;`;
+    countBtnStyle.style.cssText = `z-index: 50; opacity: 0;`;
+}
+
+// arrow function
+function plusSignFun(){
+    if(count.value >= 0){
+        minesSign.style.cssText = `display: flex;`;
+    }
+    count.value++;
+}
+
+function minesSignFun(){
+    if(count.value == 1){
+        minesSign.style.cssText = `display: none`;
+    }
+    count.value--;
+}
+
+
+// Object with array (local storge)
+
+let Order = [];
+
+if(localStorage.order != null){
+    Order = JSON.parse(localStorage.order);
+}
+else{
+    Order = [];
+}
+var OrderData = {};
+
+// add to cart btn
+
+function addToCartbtn(){
+    OrderData = {
+        itemTitle: itemTitle.innerHTML,
+        price: price.innerHTML,
+        count: count.value,
+    }
+    Order.push(OrderData);
+    localStorage.setItem('order', JSON.stringify(Order))
+    emptyCart.style.cssText = `display: none;`;
+    cartWithOrders.style.cssText = `display: flex;`;
+    showData();
+}
+
+// show data
+var cartCounter = document.getElementById('cartCounter');
+var addedOrders = document.getElementById('addedOrders');
+var total = document.getElementById('total');
+var addedOrders2 = document.getElementById('addedOrders2');
+var total2 = document.getElementById('total2');
+var cartCounter2 = document.getElementById('cartCounter2');
+
+function showData(){
+    let newElement = '';
+    let TPSumer = 0;
+    let counterSumer = 0;
+    for(let i = 0; i < Order.length; i++){
+        newElement += `
+            <div class="newElement">
+
+                <div class="newE-l-s">
+
+                    <div class="addedTitle">${Order[i].itemTitle}</div>
+
+                        <div class="newE-inf">
+
+                            <div class="addedCount">${Order[i].count}</div>
+                            <div class="addedPrice">${Order[i].price}</div>
+                            <div class="addedTotalPrice">${(Order[i].price * Order[i].count).toFixed(2)}</div>
+
+                        </div>
+
+                    </div>
+
+                <div class="newE-r-s" onclick="deleteOneItem(${i});">X</div>
+
+            </div>
+        `
+        TPSumer = +TPSumer + +(Order[i].price * Order[i].count).toFixed(2);
+        counterSumer = +counterSumer + +Order[i].count;
+    }
+    addedOrders.innerHTML = newElement;
+    total.innerHTML = TPSumer;
+    addedOrders2.innerHTML = newElement;
+    total2.innerHTML = TPSumer;
+    cartCounter.innerHTML = `&nbsp( ${counterSumer} )`;
+    tmpCountSumer = counterSumer;
+    cartCounter2.innerHTML = counterSumer;
+}
+showData();
+
+// delete one item
+
+function deleteOneItem(i){
+    Order.splice(i,1);
+    localStorage.order = JSON.stringify(Order);
+    showData();
+    if(Order.length > 0){
+        emptyCart.style.cssText = `display: none;`;
+        cartWithOrders.style.cssText = `display: flex;`;
+    }
+    else{
+        emptyCart.style.cssText = `display: flex;`;
+        cartWithOrders.style.cssText = `display: none;`;
+    }
+}
+
+// delete all items
+var deleteAllBtn = document.getElementById('deleteAll');
+
+function DeleteAll(){
+    Order.splice(0,Order.length);
+    localStorage.order = JSON.stringify(Order);
+    if(Order.length > 0){
+        emptyCart.style.cssText = `display: none;`;
+        cartWithOrders.style.cssText = `display: flex;`;
+    }
+    else{
+        emptyCart.style.cssText = `display: flex;`;
+        cartWithOrders.style.cssText = `display: none;`;
+    }
+}
+
+// confirm order
+var confirmedOrder = document.getElementById('confirmed-order');
+
+function confirmOrder(){
+    confirmedOrder.style.cssText = `display: flex`;
+}
+
+// start new Order
+
+function startNewOrder(){
+    confirmedOrder.style.cssText = `display: none`;
+    DeleteAll();
+}
+
+// scroll fun
+let cart = document.getElementById('cart');
+let mobileCart = document.getElementById('mobile-cart');
+
+mobileCart.onclick = function(){
+    scroll({
+        top: 120000000,
+        behavior: "smooth",
+    })
+}
+
+
+// Condition
+
+if(Order.length > 0){
+    emptyCart.style.cssText = `display: none;`;
+    cartWithOrders.style.cssText = `display: flex;`;
+}
+else{
+    emptyCart.style.cssText = `display: flex;`;
+    cartWithOrders.style.cssText = `display: none;`;
+}
